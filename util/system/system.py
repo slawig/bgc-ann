4914597abcb043e6f8cd2431d2f9057820ec5cfc
Markus Pfeil <mpf@informatik.uni-kiel.de>
2021-01-12 11:15:53 +0100

    Add basis function for computing on a standalone computer

--- util/system/system.py
+++ util/system/system.py
@@ -3,22 +3,23 @@
 
 import os
 import neshCluster.constants as NeshCluster_Constants
+import standaloneComputer.constants as PC_Constants
 
 BATCH_SYSTEM_ENV_NAME = 'BATCH_SYSTEM'
 
 try:
     BATCH_SYSTEM_STR = os.environ[BATCH_SYSTEM_ENV_NAME]
 except KeyError:
-    SYSTEM = 'LOGO'
-    DATA_PATH = os.path.join('/home', 'mpf', 'Dokumente', 'metos3d')
-    PYTHON_PATH = os.path.join('/home', 'mpf', 'Dokumente', 'Python', 'bgc-ann')
-    FIGURE_PATH = os.path.join('/home', 'mpf', 'Dokumente', 'Figures')
+    SYSTEM = 'PC'
+    DATA_PATH = PC_Constants.DATA_PATH
+    PYTHON_PATH = PC_Constants.PYTHON_PATH
+    FIGURE_PATH = PC_Constants.FIGURE_PATH
     BACKUP_PATH = ''
     METOS3D_MODEL_PATH = DATA_PATH 
 else:
     SYSTEM = 'NEC-NQSV'
-    PYTHON_PATH = NeshCluster_Constants.PYTHON_PATH
     DATA_PATH = NeshCluster_Constants.DATA_PATH
+    PYTHON_PATH = NeshCluster_Constants.PYTHON_PATH
     FIGURE_PATH = NeshCluster_Constants.FIGURE_PATH
     BACKUP_PATH = NeshCluster_Constants.BACKUP_PATH
     METOS3D_MODEL_PATH = DATA_PATH
