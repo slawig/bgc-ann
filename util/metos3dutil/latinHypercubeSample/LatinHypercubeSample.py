e9e2cde6c010896704a1c892ac35a6845c2b80a1
Markus Pfeil <mpf@informatik.uni-kiel.de>
2021-03-15 15:48:42 +0100

    Documentation using docstrings with numpydoc for the latin hypercube sample

--- util/metos3dutil/latinHypercubeSample/LatinHypercubeSample.py
+++ util/metos3dutil/latinHypercubeSample/LatinHypercubeSample.py
@@ -10,14 +10,32 @@ import metos3dutil.metos3d.constants as Metos3d_Constants
 
 class LatinHypercubeSample():
     """
-    Create and read the latin hypercube sample for the model parameter for the hierarchy of the biogeochemical models
-    @author: Markus Pfeil
+    Latin hypercube sample of model parameter of a biogeochemical models
     """
 
     def __init__(self, lhs_filename, noParameters=20, samples=100, criterion=None):
         """
         Initialisation of the latin hypercube sample
-        @author: Markus Pfeil
+
+        Initialisation of the latin hypercube sample for the model parameter of the hierarchy of biogeochemical models
+
+        Parameters
+        ----------
+        lhs_filename : str
+            Filename of the latin hypercube sample
+        noParameters : int, default: 20
+            Number of parameter of the latin hypercube sample
+        samples : int, default: 100
+            Number of samples in the latin hypercube sample
+        criterion : {None, 'center', 'maximin', 'centermaximin', 'correlation'}
+            Criterion how to sample the points
+
+        Attributes
+        ----------
+        _lb : numpy.ndarray
+            Numpy array with the lower bounds of the model parameter
+        _up : numpy.ndarray
+            Numpy array with the upper bounds of the model parameter
         """
         assert type(noParameters) is int and 0 < noParameters
         assert type(samples) is int and 0 < samples
@@ -42,7 +60,17 @@ class LatinHypercubeSample():
     def set_lowerBoundary(self, lowerBoundary):
         """
         Set the lower boundary for the parameter values
-        @author: Markus Pfeil
+
+        Parameters
+        ----------
+        lowerBoundary : numpy.ndarray
+            Numpy array with lower boundaries of each model parameter
+
+        Raises
+        ------
+        AssertionError
+            If the shape of lowerBoundary array does not match the count of
+            model parameter in the latin hypercube sample
         """
         assert np.shape(lowerBoundary) == np.shape(lb)
         self._lb = lowerBoundary
@@ -51,7 +79,17 @@ class LatinHypercubeSample():
     def set_upperBoundary(self, upperBoundary):
         """
         Set the upper boundary for the parameter values
-        @author: Markus Pfeil
+
+        Parameters
+        ----------
+        upperBoundary : numpy.ndarray
+            Numpy array with upper boundaries of each model parameter
+
+        Raises
+        ------
+        AssertionError
+            If the shape of upperBoundary array does not match the count of
+            model parameter in the latin hypercube sample
         """
         assert np.shape(upperBoundary) == np.shape(ub)
         self._ub = upperBoundary
@@ -60,7 +98,9 @@ class LatinHypercubeSample():
     def create(self):
         """
         Create the latin hypercube sample
-        @author: Markus Pfeil
+
+        Create the latin hypercube sample using the function lhs of the package
+        pyDOE
         """
         assert (self._noParameters,) == np.shape(self._lb)
         assert (self._noParameters,) == np.shape(self._ub)
@@ -73,7 +113,12 @@ class LatinHypercubeSample():
     def write_Lhs(self):
         """
         Write latin hypercube samples in file with big endian order
-        @author: Markus Pfeil
+
+        Raises
+        ------
+        AssertionError
+            If a file with the set filename of the latin hypercube sample
+            already exists.
         """
         samples = np.array(self._samples, dtype = '>i4')
         noParameters = np.array(self._noParameters, dtype = '>i4')
@@ -92,8 +137,14 @@ class LatinHypercubeSample():
     def read_Lhs(self):
         """
         Read latin hypercube sample out file in big endian order
-        @author: Markus Pfeil
+
+        Raises
+        ------
+        AssertionError
+            If the file with the set filename of the latin hypercube sample
+            does not exist.
         """
+        assert os.path.exists(self._lhs_filename)
         fid = open(self._lhs_filename, 'rb')
         self._samples, = np.fromfile(fid, dtype = '>i4', count = 1)
         self._noParameters, = np.fromfile(fid, dtype = '>i4', count = 1)
@@ -105,29 +156,59 @@ class LatinHypercubeSample():
 
     def get_sample_count(self):
         """
-        Get the number of samples
-        @author: Markus Pfeil
+        Returns the number of samples
+
+        Returns
+        -------
+        int
+            Number of samples
         """
         return self._samples
 
 
     def get_all_parameter(self, lhs_index):
         """
-        Get all model parameters for the given index of the latin hypercube sample
-        @author: Markus Pfeil
+        Returns all model parameters
+
+        Returns all model parameters of the hierarchy for the given index
+
+        Parameters
+        ----------
+        lhs_index : int
+            Id of the parameter of the latin hypercube example
+
+        Raises
+        ------
+        AssertionError
+            If the lhs_index is greater or equal than the number of samples.
         """
         assert type(lhs_index) is int and 0 <= lhs_index and lhs_index < self._samples
 
         return self._lhs[:, lhs_index]
 
 
-    def get_parameter(self, model, lhs_index):
+    def get_parameter(self, metos3dModel, lhs_index):
         """
-        Get the model parameter for the given model and index of the latin hypercube sample
+        Returns the model parameter for the given model
+
+        Returns the model parameter for the given model of the hierarchy for
+        the given index
+
+        Parameters
+        ----------
+        metos3dModel : str
+            Name of the biogeochemical model
+        lhs_index : int
+            Id of the parameter of the latin hypercube example
+
+        Raises
+        ------
+        AssertionError
+            If the lhs_index is greater or equal than the number of samples.
         @author: Markus Pfeil
         """
-        assert model in Metos3d_Constants.METOS3D_MODELS
+        assert metos3dModel in Metos3d_Constants.METOS3D_MODELS
         assert type(lhs_index) is int and 0 <= lhs_index and lhs_index < self._samples
 
-        return self.get_all_parameter(lhs_index)[Metos3d_Constants.PARAMETER_RESTRICTION[model]]
+        return self.get_all_parameter(lhs_index)[Metos3d_Constants.PARAMETER_RESTRICTION[metos3dModel]]
 
